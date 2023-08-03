0df4ffdee0c83c8008d6592e1cad39461a4f8957
Marcos Vinícius <123216797+Marcos-vsa@users.noreply.github.com>
2023-08-03 12:07:40 -0300

    ex01

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-console.log("olá mundo")
+const prompt = require("prompt-sync")();
 
-function soma (x,y){
-  console.log(x,y)
+const nome = prompt("Digite seu nome: ")
+const idade = prompt("Digite seu idade: ")
+
+class Pessoa {
+  constructor(nome,idade){
+    this.nome = nome 
+    this.idade = idade
+  }
+  apresentar(){
+    console.log(`Oi , meu nome é ${this.nome} e tenho ${this.idade} anos .`);
+  }
 }
+
+const pessoa1 = new Pessoa(nome,idade);
+pessoa1.apresentar()
+
+class Aluno extends Pessoa{
+  constructor(nome,idade,matricula){
+  //super serve para herdar atributos da classe pai 
+    super(nome,idade)
+    this.matricula = matricula; 
+  }
+}
+
+const aluno1 = new Aluno(nome,idade,232425)
+console.log(aluno1)
